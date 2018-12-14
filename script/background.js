dc0d52d007b5dba9c4a089675344eee9d1ca5258
Eduard Karpa <31408020+edmlbox@users.noreply.github.com>
2018-12-14 22:25:38 +0200

    Update background.js

--- script/background.js
+++ script/background.js
@@ -1,21 +1,19 @@
-chrome.runtime.onMessage.addListener(
- 
-    function (request, sender, sendResponse) {
-        var sendto = 'empty';
-        var ccc = new XMLHttpRequest();
-        ccc.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText)
-                sendto = this.responseText;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  var sendto = "empty";
+  var ccc = new XMLHttpRequest();
+  ccc.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      sendto = this.responseText;
+    }
+  };
+  ccc.open(
+    "GET",
+    "http://cryptobit-env.7hiybanifg.eu-central-1.elasticbeanstalk.com/" +
+      request.greeting,
+    false
+  );
+  ccc.send();
 
-
-
-
-            }
-        }
-        ccc.open("GET", "http://cryptobit-env.7hiybanifg.eu-central-1.elasticbeanstalk.com/" + request.greeting,false)
-        ccc.send();
-
-        sendResponse({ helloWorld: sendto });
-
-    });
+  sendResponse({ helloWorld: sendto });
+});
